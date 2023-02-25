7104a69e09208c89af39138b629ed89c9ddf5cfd
Issou83 <issou8@gmail.com>
2023-02-25 20:05:53 +0100

    Integration des images banner dans le scss pour une meilleure gestion de leur opacité

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import imageHome from "../assets/images/IMG.png";
-import imageAbout from "../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash.png";
+// import imageHome from "../assets/images/IMG.png";
+// import imageAbout from "../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash.png";
 
 
 const Banner = () => {
@@ -18,10 +18,10 @@ const Banner = () => {
     console.log(location);
 
     return (
-        <div className='banner'>
-            { locationHome ? 
+        <div className={locationHome ? 'banner banner__home' :'banner banner__about'}>
+            {/* { locationHome ? 
             <img src={imageHome} className="banner__img" alt="banniére"/> :
-            <img src={imageAbout} className="banner__img" alt="banniére"/> }
+            <img src={imageAbout} className="banner__img" alt="banniére"/> } */}
             <h1 className='banner__title'>Chez vous, partout et ailleurs</h1>
         </div>
         );
