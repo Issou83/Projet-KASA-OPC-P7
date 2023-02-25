489d72d46e3312948446c362308d24304234b12a
Issou83 <issou8@gmail.com>
2023-02-25 23:03:22 +0100

    Ajout d'une animation au hover des cartes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,7 +20,6 @@ const Banner = () => {
     return (
         <div className={locationHome ? 'banner banner__home' :'banner banner__about'}>
             {locationHome ? <h1 className='banner__title'>Chez vous, partout et ailleurs</h1> : ""}
-            
         </div>
         );
     }
