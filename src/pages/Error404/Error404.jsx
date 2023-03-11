ead73a2c43bab167520670e77f78a15ec71fc285
Issou83 <issou8@gmail.com>
2023-03-11 13:06:42 +0100

    Style et responsive réalisés sur le footer (global) et la page Erreur404 + résolution de l'ancrage du footer au pied de page quelque soit la taille d'ecran (application de GRID)

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './index.scss';
+import { NavLink } from 'react-router-dom';
+import errorPage from "../../assets/images/404.png"
 
 const Error404 = () => {
     return (
-        <main>
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
+        <main className='main404'>
+            <div className='containerError'>
+                <img src={errorPage} alt="" />
+                <h1>Oups! La page que vous&nbsp;demandez&nbsp;n'existe&nbsp;pas.</h1>
+                <NavLink to="/">
+                    <p>Retourner sur la page d'accueil</p>
+                </NavLink>
+            </div>
         </main>
     );
 };
